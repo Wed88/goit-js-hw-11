12b0036329e099d2d07852b836181a1bca7861db
Dmitriy Dudnik <wed2288@gmail.com>
2021-11-23 00:28:20 +0200

    добавляет Notify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import './sass/main.scss';
-
 import PixabayApiService from './js/pixabay-service';
 import photoCardMarkupTpl from './templates/photoCardMarkup';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   searchFormEl: document.querySelector('.search-form'),
@@ -16,17 +15,36 @@ refs.loadMoreBtnEl.addEventListener('click', onLoadMore);
 
 refs.loadMoreBtnEl.classList.add('is-hidden');
 
-function onSearch(e) {
+async function onSearch(e) {
   e.preventDefault();
+  pixabayApiService.searchQuery = e.currentTarget.elements.searchQuery.value;
+
+  const hits = await pixabayApiService.fetchImg();
+  const totalHits = hits.data.totalHits;
+
+  if (totalHits < 1) {
+    clearPhotoCardGallery();
+    refs.loadMoreBtnEl.classList.add('is-hidden');
+    Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    return;
+  }
 
   clearPhotoCardGallery();
-  pixabayApiService.searchQuery = e.currentTarget.elements.searchQuery.value;
+
   pixabayApiService.resetPage();
   pixabayApiService.fetchImg().then(appendPhotoCardMarkup);
   refs.loadMoreBtnEl.classList.remove('is-hidden');
 }
 
-function onLoadMore() {
+async function onLoadMore() {
+  const hits = await pixabayApiService.fetchImg();
+  const hitsLength = hits.data.hits.length;
+
+  if (hitsLength < 40) {
+    refs.loadMoreBtnEl.classList.add('is-hidden');
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    return;
+  }
   pixabayApiService.fetchImg().then(appendPhotoCardMarkup);
 }
 
